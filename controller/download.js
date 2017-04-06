2c72a4a0c7f26f8a54bdfd7636a53c7b3964b0fd
liuwensa <liu1028hmily@163.com>
2017-04-06 17:34:02 +0800

    增加下载的返回值

--- controller/download.js
+++ controller/download.js
@@ -16,7 +16,36 @@ module.exports = {
 /**
  * @method 下载图片 /download/images - GET
  * @param {String} req.query.urls  下载图片数组，JSON.stringify()后数组字符串
- * @returns {*}
+ * @returns {*} 返回结果：
+ * ```json
+ * {
+ *    "code": 0,
+ *    "msg": [
+ *        {
+ *            "imageUrl": "http://127.0.0.1:20008",
+ *            "url": "/images/40/34/4034807e7e6042e8bb83d6faf512d174-56776-600x400.jpeg",
+ *            "fileSize": 56776,
+ *            "size": {
+ *                "width": 600,
+ *                "height": 400
+ *            },
+ *            "format": "JPEG",
+ *            "originUrl": "http://img.357.com/wjgame/up/lo/upload_507e2d290ddbae3328d1cb02e7f2005d.jpg_450x450.jpg"
+ *        },
+ *        {
+ *            "imageUrl": "http://127.0.0.1:20008",
+ *            "url": "/images/10/15/1015eadd83a5430f9679ee14f30964da-61660-600x400.jpeg",
+ *            "fileSize": 61660,
+ *            "size": {
+ *                "width": 600,
+ *                "height": 400
+ *            },
+ *            "format": "JPEG",
+ *            "originUrl": "http://img.357.com/wjgame/up/lo/upload_2f59a8f4f17d3b6a59fb6efb4d59ec39.jpg_450x450.jpg"
+ *        }
+ *    ]
+ *}
+ * ```
  */
 function downloadImages(req, res) {
   return image.handleImages(req.files)
